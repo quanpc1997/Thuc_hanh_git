e5f6eb0a5b00233a39473aca94392b0f506a8bb4
Phan Quan <d9pcquan@cmcglobal.vn>
2023-02-11 22:29:49 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int nhan(int &a, int &b) {
+    
+
+
+}
+
+
+int tru(int &a, int &b) {
+    
+
+
+}
+
+
+int cong(int &a, int &b) {
+    
+
+
+
+}
+
+int main() {
+    
+    return 0
+}
